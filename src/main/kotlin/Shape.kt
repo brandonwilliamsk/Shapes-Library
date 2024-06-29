ebd121b74695883dac3d0363e7d4dbb63a55835d
brandonwilliamsk <brandonwilliams.kent@gmail.com>
2024-06-29 10:12:51 -0600

    point, line, shape abstract class rough drafts added.

---
+++ src/main/kotlin/Shape.kt
@@ -0,0 +1,12 @@
+package org.example
+
+abstract class Shape {
+    abstract fun move(deltaX: Double, deltaY: Double)
+    abstract fun getPoints(): List<Point>
+    protected abstract fun calculateArea(): Double
+    fun getArea(): Double {
+        val area = calculateArea()
+        if (area == 0.0) throw IllegalStateException("Area cannot be zero")
+        return area
+    }
+}
---
+++ src/main/kotlin/point.kt
@@ -0,0 +1,11 @@
+package org.example
+
+class Point(private var x: Double, private var y: Double) {
+    fun clone(): Point = Point(x, y)
+    fun getX(): Double = x
+    fun getY(): Double = y
+    fun move(deltaX: Double, deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
