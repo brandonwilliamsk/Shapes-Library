f7c16ed9002bcf51d66fe490a36d819b495b167c
brandonwilliamsk <brandonwilliams.kent@gmail.com>
2024-06-30 13:10:05 -0600

    implemented triangle class.

--- src/main/kotlin/Triangle.kt
+++ src/main/kotlin/Triangle.kt
@@ -1,4 +1,20 @@
 package shapes
 
-class Triangle {
+class Triangle(private val pointA: Point, private val pointB: Point, private val pointC: Point) : Shape() {
+    init {
+        val area = AreaCalculator.triangleArea(pointA, pointB, pointC)
+        if (area == 0.0) {
+            throw IllegalArgumentException("A triangle cannot have an area of 0")
+        }
+    }
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        pointA.move(deltaX, deltaY)
+        pointB.move(deltaX, deltaY)
+        pointC.move(deltaX, deltaY)
+    }
+
+    override fun getPoints(): List<Point> = listOf(pointA, pointB, pointC)
+
+    override fun calculateArea(): Double = AreaCalculator.triangleArea(pointA, pointB, pointC)
 }
