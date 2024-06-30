531da58bc9efaa5d9bdb9a5f342b46bfecd227d6
brandonwilliamsk <brandonwilliams.kent@gmail.com>
2024-06-30 11:08:48 -0600

    updated Rectangle class

--- src/main/kotlin/Rectangle.kt
+++ src/main/kotlin/Rectangle.kt
@@ -1,4 +1,24 @@
 package shapes
 
-class Rectangle {
+import kotlin.math.abs
+
+class Rectangle(private val topLeft: Point, private val bottomRight: Point) : Shape() {
+    init {
+        if (topLeft.getX() == bottomRight.getX() || topLeft.getY() == bottomRight.getY()) {
+            throw IllegalArgumentException("A rectangle cannot have 0 width or height")
+        }
+    }
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        topLeft.move(deltaX, deltaY)
+        bottomRight.move(deltaX, deltaY)
+    }
+
+    override fun getPoints(): List<Point> = listOf(topLeft, bottomRight)
+
+    override fun calculateArea(): Double {
+        val width = abs(bottomRight.getX() - topLeft.getX())
+        val height = abs(bottomRight.getY() - topLeft.getY())
+        return AreaCalculator.rectangleArea(width, height)
+    }
 }
