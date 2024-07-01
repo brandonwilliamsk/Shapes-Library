8fc119790e39a0c4e8d4722ec47b2bf0d23e6276
brandonwilliamsk <brandonwilliams.kent@gmail.com>
2024-06-30 18:13:13 -0600

    circle and ellipse tests updated

--- src/test/kotlin/shapesTest/CircleTests.kt
+++ src/test/kotlin/shapesTest/CircleTests.kt
@@ -1,4 +1,28 @@
 package shapesTest
+import shapes.*
+import geometry.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class CircleTests {
+class CircleTest {
+
+    @Test
+    fun testGetPoints() {
+        val circle = Circle(Point(2.0, 3.0), 5.0)
+        val points = circle.getPoints()
+        assertEquals(Point(2.0, 3.0), points[0])
+    }
+
+    @Test
+    fun testGetArea() {
+        val circle = Circle(Point(2.0, 3.0), 5.0)
+        assertEquals(Math.PI * 5.0 * 5.0, circle.getArea())
+    }
+
+    @Test
+    fun testMove() {
+        val circle = Circle(Point(2.0, 3.0), 5.0)
+        circle.move(1.0, 1.0)
+        assertEquals(Point(3.0, 4.0), circle.getPoints()[0])
+    }
 }
--- src/test/kotlin/shapesTest/EllipseTests.kt
+++ src/test/kotlin/shapesTest/EllipseTests.kt
@@ -1,4 +1,28 @@
 package shapesTest
+import shapes.*
+import geometry.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class EllipseTests {
+class EllipseTest {
+
+    @Test
+    fun testGetPoints() {
+        val ellipse = Ellipse(Point(2.0, 3.0), 4.0, 5.0)
+        val points = ellipse.getPoints()
+        assertEquals(Point(2.0, 3.0), points[0])
+    }
+
+    @Test
+    fun testGetArea() {
+        val ellipse = Ellipse(Point(2.0, 3.0), 4.0, 5.0)
+        assertEquals(Math.PI * 4.0 * 5.0, ellipse.getArea())
+    }
+
+    @Test
+    fun testMove() {
+        val ellipse = Ellipse(Point(2.0, 3.0), 4.0, 5.0)
+        ellipse.move(1.0, 1.0)
+        assertEquals(Point(3.0, 4.0), ellipse.getPoints()[0])
+    }
 }
