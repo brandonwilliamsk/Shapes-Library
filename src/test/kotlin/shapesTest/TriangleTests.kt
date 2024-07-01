1f4287a1eb8700693319995a424770ffda81881d
brandonwilliamsk <brandonwilliams.kent@gmail.com>
2024-06-30 18:35:51 -0600

    triangle test updated

--- src/test/kotlin/shapesTest/TriangleTests.kt
+++ src/test/kotlin/shapesTest/TriangleTests.kt
@@ -1,4 +1,32 @@
 package shapesTest
+import shapes.*
+import geometry.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class TriangleTests {
+class TriangleTest {
+
+    @Test
+    fun testGetPoints() {
+        val triangle = Triangle(Point(0.0, 0.0), Point(4.0, 0.0), Point(0.0, 3.0))
+        val points = triangle.getPoints()
+        assertEquals(Point(0.0, 0.0), points[0])
+        assertEquals(Point(4.0, 0.0), points[1])
+        assertEquals(Point(0.0, 3.0), points[2])
+    }
+
+    @Test
+    fun testGetArea() {
+        val triangle = Triangle(Point(0.0, 0.0), Point(4.0, 0.0), Point(0.0, 3.0))
+        assertEquals(6.0, triangle.getArea())
+    }
+
+    @Test
+    fun testMove() {
+        val triangle = Triangle(Point(0.0, 0.0), Point(4.0, 0.0), Point(0.0, 3.0))
+        triangle.move(1.0, 1.0)
+        assertEquals(Point(1.0, 1.0), triangle.getPoints()[0])
+        assertEquals(Point(5.0, 1.0), triangle.getPoints()[1])
+        assertEquals(Point(1.0, 4.0), triangle.getPoints()[2])
+    }
 }
